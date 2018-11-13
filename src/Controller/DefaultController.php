de534f79fc5832f5e16a96fca06e0708446dce89
Mischa Zöller <dev@zoeller.biz>
2018-11-13 16:50:24 +0100

    Doctrine MongoDB added.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use MongoDB\Client;
@@ -11,11 +12,16 @@ class DefaultController extends Controller
     public function index()
     {
         if (key_exists('MONGODB_URL', $_ENV)) {
+            /** @var DocumentManager $dm */
             $dm = $this->get('doctrine_mongodb')->getManager();
+            $repo = $dm->getRepository(Starwars::class);
+            $starwars = $repo->findAll();
+            $result = count($starwars);
 
-            $client = new Client($_ENV['MONGODB_URL']);
-            $collection = $client->main->starwars;
-            $result = $collection->count();
+//            $client = new Client($_ENV['MONGODB_URL']);
+//            $collection = $client->main->starwars;
+//            $result = $collection->count();
+
             return new Response("Number of objects in collection 'starwars': {$result}");
         } else {
             return new Response("Environment variable MONGODB_URL not found.", 404);
